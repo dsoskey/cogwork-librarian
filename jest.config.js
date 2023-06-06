78005c44c14bed042e1198aeee2bce4a4bde1db9
Daniel Soskey <asks@danielsoskey.com>
2023-06-05 20:05:30 -0700

    fixed coverage reporting

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   testEnvironment: 'jsdom',
   testMatch: ["**/src/**/*.test.[jt]s", '**/test/**/*.test.[jt]s'],
   // collectCoverage: true,
+  coverageReporters: ["html", "text"],
   transform: {
     'tsx?$': ['@swc/jest'],
     "^.+\\.ne$": "jest-transform-nearley",
