46c6e6a5eee37c6155836a3f38694afa7bfa908d
Daniel Soskey <asks@danielsoskey.com>
2023-08-28 17:31:27 -0700

    added weight column to cardListView

--- src/ui/cardBrowser/cardViews/cardListView.tsx
+++ src/ui/cardBrowser/cardViews/cardListView.tsx
@@ -1,8 +1,9 @@
-import { EnrichedCard } from '../../../api/queryRunnerCommon'
-import React from 'react'
+import { EnrichedCard, SCORE_PRECISION } from '../../../api/queryRunnerCommon'
+import React, { useContext } from 'react'
 import './cardListView.css'
 import { ManaSymbol, toSplitCost } from '../../../api/memory/types/card'
 import { ManaIcon } from '../../card/manaSymbol'
+import { FlagContext } from '../../../flags'
 
 interface CardListRowProps {
   card: EnrichedCard
@@ -10,6 +11,7 @@ interface CardListRowProps {
 
 
 export const CardListRow = ({ card }: CardListRowProps) => {
+  const { showDebugInfo } = useContext(FlagContext).flags
 
   return <tr>
     <td>
@@ -21,6 +23,7 @@ export const CardListRow = ({ card }: CardListRowProps) => {
       {card.data.mana_cost && toSplitCost(card.data.mana_cost).map(it => <ManaIcon symbol={it as ManaSymbol} />)}
     </td>
     <td>{card.data.type_line}</td>
+    {showDebugInfo && card.weight.toPrecision(SCORE_PRECISION)}
   </tr>
 }
 
@@ -29,13 +32,14 @@ interface CardListViewProps {
 }
 
 export const CardListView = ({ result }: CardListViewProps) => {
-
+  const { showDebugInfo } = useContext(FlagContext).flags
   return <table className='list-view'>
     <thead>
       <tr>
         <th>name</th>
         <th>mana cost</th>
         <th>type</th>
+        {showDebugInfo && <th>weight</th>}
       </tr>
     </thead>
     <tbody>
