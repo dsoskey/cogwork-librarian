4301594debb0ff0a56b3006dcc6e929bf4b53560
Daniel Soskey <asks@danielsoskey.com>
2024-05-18 10:38:57 -0700

    useEffect is now defined!

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { CogDBContext, useCogDB } from './api/local/useCogDB'
 import { Footer } from './ui/footer'
 import { AppInfo } from './ui/appInfo'
